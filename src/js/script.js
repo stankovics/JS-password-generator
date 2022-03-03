7b08e60afdc5e9ff200cdb60a9b35d8c99ab665d
stankovics <stankovicnstefan@gmail.com>
2022-03-02 22:08:55 -0600

    arrayFromLowToHigh()

--- src/js/script.js
+++ src/js/script.js
@@ -5,3 +5,18 @@ const characterAmountRange = document.getElementById('characterAmountRange');
 const includeUppercaseElement = document.getElementById('includeUppercase');
 const includeNumbersElement = document.getElementById('includeNumbers');
 const includeSymbolsElement = document.getElementById('includeSymbols');
+
+const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90);
+const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122);
+const NUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57);
+const SYMBOL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(
+  arrayFromLowToHigh(58, 64).concat(91, 96).concat(123, 126)
+);
+
+function arrayFromLowToHigh(low, high) {
+  const array = [];
+  for (let i = low; i <= high; i++) {
+    array.push(i);
+  }
+  return array;
+}

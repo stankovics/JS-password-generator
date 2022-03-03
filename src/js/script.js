5c6af7c884023187e33aa61ed6f36b4ac18c12fd
stankovics <stankovicnstefan@gmail.com>
2022-03-02 20:46:25 -0600

    select js elements

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+const passwordDisplay = document.getElementById('passwordDisplay');
+const form = document.getElementById('passwordGeneratorForm');
+const characterAmountNumber = document.getElementById('characterAmountNumber');
+const characterAmountRange = document.getElementById('characterAmountRange');
+const includeUppercaseElement = document.getElementById('includeUppercase');
+const includeNumbersElement = document.getElementById('includeNumbers');
+const includeSymbolsElement = document.getElementById('includeSymbols');
